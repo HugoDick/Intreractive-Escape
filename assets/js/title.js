1380acb6c4bae63f4d5583acdc0ea4281e8b5177
Hugo DICK <hugo.dick2007@gmail.com>
2026-05-08 10:11:11 +0200

    Patron de la page d'accueil

---
+++ assets/js/title.js
@@ -0,0 +1,4 @@
+const text = document.querySelectorAll('div.wave label');
+text.forEach(label =>{
+    label.innerHTML = label.textContent.split('').map((text, wave) => `<span style="transition-delay: ${wave * 25}ms">${text}</span>`).join('');
+});
